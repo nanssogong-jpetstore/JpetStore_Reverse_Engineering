e124e0f7e02df5ea2fec1f1b3309d817325b4a7f
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2022-11-22 22:47:01 +0900

    Test : "테스트"

--- src/main/java/org/mybatis/jpetstore/domain/Account.java
+++ src/main/java/org/mybatis/jpetstore/domain/Account.java
@@ -26,7 +26,6 @@ import net.sourceforge.stripes.validation.Validate;
  */
 public class Account implements Serializable {
 //134
-  ////123124124124
   private static final long serialVersionUID = 8751282105532159742L;
 
   private String username;
