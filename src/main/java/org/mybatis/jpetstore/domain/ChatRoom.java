63ee5215e245ed5dacd02b4c6f623eab637c3f1a
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2022-11-24 20:16:53 +0900

    feat : chat DB Mapper

--- src/main/java/org/mybatis/jpetstore/domain/ChatRoom.java
+++ src/main/java/org/mybatis/jpetstore/domain/ChatRoom.java
@@ -1,27 +1,47 @@
 package org.mybatis.jpetstore.domain;
 
-import java.util.UUID;
 
 public class ChatRoom {
 
-    private String roomId;
+    private int roomId;
+    private String userId;
     private String roomName;
+    private String imgurl;
     private int isRead;
 
     public ChatRoom() {
 
     }
-    public ChatRoom(String roomId, String roomName, int isRead) {
+
+    public ChatRoom(int roomId, String userId, String roomName, String imgurl, int isRead) {
         this.roomId = roomId;
+        this.userId = userId;
         this.roomName = roomName;
+        this.imgurl = imgurl;
         this.isRead = isRead;
     }
 
-    public String getRoomId() {
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public int getRoomId() {
         return roomId;
     }
 
-    public void setRoomId(String roomId) {
+    public void setRoomId(int roomId) {
         this.roomId = roomId;
     }
 
@@ -41,11 +61,5 @@ public class ChatRoom {
         this.isRead = isRead;
     }
 
-    public static ChatRoom create(String name) {
-        ChatRoom room = new ChatRoom();
-        room.roomId = UUID.randomUUID().toString();
-        room.roomName = name;
-        return room;
-    }
 
 }
