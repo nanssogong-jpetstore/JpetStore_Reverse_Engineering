d561e6610021f3b7d2fbf027a443357b3e6823f8
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2022-11-26 19:45:02 +0900

    Refactor : "AnimalMating" 에러처리

--- src/main/java/org/mybatis/jpetstore/web/actions/AnimalActionBean.java
+++ src/main/java/org/mybatis/jpetstore/web/actions/AnimalActionBean.java
@@ -1,22 +1,15 @@
 package org.mybatis.jpetstore.web.actions;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import net.sourceforge.stripes.action.FileBean;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.SessionScope;
 import net.sourceforge.stripes.integration.spring.SpringBean;
-import org.mybatis.jpetstore.configuration.AWSS3;
 import org.mybatis.jpetstore.domain.AnimalMating;
 import org.mybatis.jpetstore.service.AnimalService;
 import org.mybatis.jpetstore.service.CatalogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpSession;
 import java.io.File;
@@ -25,7 +18,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 @SessionScope
 public class AnimalActionBean extends AbstractActionBean {
@@ -87,10 +79,6 @@ public class AnimalActionBean extends AbstractActionBean {
 
 
 
-    @Autowired
-    public AWSS3 awsS3 = AWSS3.getInstance();
-
-    private String bucketName="jpet-img";
 
 
 
@@ -107,8 +95,25 @@ public class AnimalActionBean extends AbstractActionBean {
     public Resolution uploadImg() throws Exception {
         HttpSession session = context.getRequest().getSession();
         AccountActionBean accountBean = (AccountActionBean) session.getAttribute("/actions/Account.action");
+        if(animalMating.getTitle()==null){
+            setMessage("PLEASE ENTER A TITLE");
+            return new ForwardResolution(ERROR);
+        }
+        if(animalMating.getContents()==null){
+            setMessage("PLEASE ENTER CONTENTS");
+            return new ForwardResolution(ERROR);
+        }if(animalMating.getCharacters()==null){
+            setMessage("PLEASE ENTER CHARACTERS");
+            return new ForwardResolution(ERROR);
+        }if(fileBean==null){
+            setMessage("PLEASE POST FILE IMG");
+            return new ForwardResolution(ERROR);
+        }
+
+
+
         String userId=accountBean.getUsername();
-        String url=uploadImgFile();
+        String url=animalService.uploadImgFile(fileBean);
         animalMating.setImgUrl(url);
         animalMating.setUserId(userId);
         animalService.insertAnimal(animalMating);
@@ -133,52 +138,6 @@ public class AnimalActionBean extends AbstractActionBean {
         return new ForwardResolution(DETAIL_ANIMAL_MATING);
     }
 
-    private String uploadImgFile() throws IOException {
-        try {
-            System.out.println(fileBean.getFileName());
-            String fName = fileBean.getFileName();
-            System.out.println(fName.indexOf("."));
-
-            if (fName.indexOf(".") != -1) {
-                String ext = fName.split("\\.")[1];
-                String contentType="";
-                switch (ext) {
-                    case "jpeg":
-                        contentType = "image/jpeg";
-                        break;
-                    case "png":
-                        contentType = "image/png";
-                        break;
-                    case "txt":
-                        contentType = "text/plain";
-                        break;
-                    case "csv":
-                        contentType = "text/csv";
-                        break;
-                }
-
-                ObjectMetadata metadata=new ObjectMetadata();
-                metadata.setContentType(contentType);
-                PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, UUID.randomUUID() + "." + ext, fileBean.getInputStream(),metadata);
-                putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead);
-                awsS3.uploadToS3(putObjectRequest);
-                logger.info("===================== Upload File - Done! =====================");
-                return "https://jpet-img.s3.ap-northeast-2.amazonaws.com/"+putObjectRequest.getKey();
-
-            }
-        } catch (AmazonServiceException ase) {
-            logger.info("Caught an AmazonServiceException from PUT requests, rejected reasons:");
-            logger.info("Error Message:    " + ase.getMessage());
-            logger.info("HTTP Status Code: " + ase.getStatusCode());
-            logger.info("AWS Error Code:   " + ase.getErrorCode());
-            logger.info("Error Type:       " + ase.getErrorType());
-            logger.info("Request ID:       " + ase.getRequestId());
-        } catch (AmazonClientException ace) {
-            logger.info("Caught an AmazonClientException: ");
-            logger.info("Error Message: " + ace.getMessage());
-        }
-        return null;
-    }
 
 
 
