5038df466b014a4268e23c1dd80e879d4774432d
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2022-11-30 16:32:34 +0900

    feact : new Message

--- src/main/java/org/mybatis/jpetstore/Chat/ChatService.java
+++ src/main/java/org/mybatis/jpetstore/Chat/ChatService.java
@@ -1,6 +1,7 @@
 package org.mybatis.jpetstore.Chat;
 
 import org.mybatis.jpetstore.domain.ChatMessage;
+import org.mybatis.jpetstore.domain.ChatRoomUser;
 import org.mybatis.jpetstore.mapper.ChatMessageMapper;
 import org.mybatis.jpetstore.mapper.ChatMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,37 @@ public class ChatService {
     @Transactional
     public void insertMessage(ChatMessage chatMessage) {
         chatMapper.insertMessage(chatMessage);
+    }
+
+    @Transactional
+    public void enterRoom(ChatMessage chatMessage) {
+        ChatRoomUser chatRoomUser = new ChatRoomUser();
+        chatRoomUser.setRoomId(chatMessage.getRoomId());
+        chatRoomUser.setUserId(chatMessage.getSender());
+        chatMapper.enterRoom(chatRoomUser);
+    }
+    public void exitRoom(ChatMessage chatMessage) {
+        ChatRoomUser chatRoomUser = new ChatRoomUser();
+        chatRoomUser.setRoomId(chatMessage.getRoomId());
+        chatRoomUser.setUserId(chatMessage.getSender());
+        chatMapper.exitRoom(chatRoomUser);
+    }
+
+    public ChatRoomUser getReceiverStatus(ChatMessage chatMessage) {
+        return chatMapper.getChatRoomUser(chatMessage);
+    }
+
+    public void readMessage(ChatMessage chatMessage) {
+        ChatRoomUser chatRoomUser = new ChatRoomUser();
+        chatRoomUser.setUserId(chatMessage.getSender());
+        chatRoomUser.setRoomId(chatMessage.getRoomId());
+        chatMapper.readMessage(chatRoomUser);
+    }
+
+    public void noneReadMessage(ChatMessage chatMessage) {
+        ChatRoomUser chatRoomUser = new ChatRoomUser();
+        chatRoomUser.setUserId(chatMessage.getReceiver());
+        chatRoomUser.setRoomId(chatMessage.getRoomId());
+        chatMapper.noneReadMessage(chatRoomUser);
     }
 }
